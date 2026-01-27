745e51c47d65e0f0f86b2d2c6a0c65617a686f8e
kolen44 <malavov70@gmail.com>
2026-01-27 17:18:57 +0100

    Increase max phrases to 50

--- runva_vosk.py
+++ runva_vosk.py
@@ -35,7 +35,7 @@ WAKE_WORDS = [
 
 # Smart Turn - настройки умного склеивания фраз
 SMART_TURN_TIMEOUT = 7.0  # секунд ждать продолжения после wake word
-SMART_TURN_MAX_PHRASES = 10  # максимум фраз для склеивания
+SMART_TURN_MAX_PHRASES = 50  # максимум фраз для склеивания (практически без лимита)
 
 import time
 
